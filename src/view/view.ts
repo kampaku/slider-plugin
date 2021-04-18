99ee38a46bd83e3655e8ef11bc041298097cbfb6
kampaku <43931529+kampaku@users.noreply.github.com>
2021-04-18 19:44:03 +0300

    add vertical align

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,4 +1,5 @@
 import createElement from '../helpers/create-element';
+import type { ModelInterface } from '../model/modelInterface';
 import Track from './track';
 import Thumb from './thumb';
 
@@ -7,12 +8,38 @@ export default class View {
   thumb: Thumb;
   track: Track;
   slider: HTMLElement;
+
   constructor() {
     this.app = document.querySelector('#root');
-    this.slider = createElement('div', 'slider');
+    this.slider = createElement('div');
     this.thumb = new Thumb();
     this.track = new Track();
+  }
+
+  render(props: ModelInterface) {
+    const {
+      min,
+      max,
+      step,
+      from,
+      to,
+      vertical,
+      tip,
+      range,
+      connect,
+      scale,
+    } = props;
     this.app?.append(this.slider);
+    if (vertical) {
+      this.slider.classList.add('slider-vertical');
+      this.track.element.classList.add('track-vertical');
+      this.thumb.element.classList.add('thumb-vertical');
+    } else {
+      this.slider.classList.add('slider');
+      this.track.element.classList.add('track');
+      this.thumb.element.classList.add('thumb-horizontal');
+    }
+
     this.slider.append(this.track.element, this.thumb.element);
   }
 }
