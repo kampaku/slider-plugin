f0590d90377827c34b6fd3c36c5294bcd3b93206
kampaku <43931529+kampaku@users.noreply.github.com>
2021-04-18 19:41:36 +0300

    add interface for model

---
+++ src/model/modelInterface.ts
@@ -0,0 +1,12 @@
+export interface ModelInterface {
+  min: number;
+  max: number;
+  step: number;
+  from: number;
+  to: number;
+  vertical: boolean;
+  tip: boolean;
+  range: boolean;
+  connect: boolean;
+  scale: boolean;
+}
