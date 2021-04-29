d31598a7e3159472c1704705286d70d94546af92
kampaku <43931529+kampaku@users.noreply.github.com>
2021-04-29 23:43:21 +0300

    fix calculate position

--- src/presenter/presenter.ts
+++ src/presenter/presenter.ts
@@ -72,8 +72,15 @@ export default class Presenter {
   calculatePosition(value: number) {
     const arr = this.model.getValueArray();
     const index = arr.indexOf(value);
-    const position = (100 / arr.length) * index + '%';
-    return position;
+
+    const { offset } = this.getOrientation();
+    const maxWidthWithoutThumb =
+      100 -
+      (this.view.thumbFrom.element[offset] /
+        this.view.sliderContainer[offset]) *
+        100;
+    const position = (maxWidthWithoutThumb / (arr.length - 1)) * index;
+    return Math.round(position) + '%';
   }
 
   getOrientation() {
@@ -81,7 +88,7 @@ export default class Presenter {
       clientXY: this.model.vertical ? 'clientY' : 'clientX',
       start: this.model.vertical ? 'top' : 'left',
       end: this.model.vertical ? 'bottom' : 'right',
-      offsetWH: this.model.vertical ? 'offsetHeight' : 'offsetWidth',
+      offset: this.model.vertical ? 'offsetHeight' : 'offsetWidth',
     };
   }
 
@@ -89,18 +96,21 @@ export default class Presenter {
     const { start, end } = this.getOrientation();
     let startPosition = this.calculatePosition(this.model.getFrom());
     let endPosition = this.calculatePosition(this.model.getTo());
+    if (parseInt(startPosition) > parseInt(endPosition)) {
+      [endPosition, startPosition] = [startPosition, endPosition];
+    }
     if (this.model.getRange()) {
       endPosition = 100 - parseInt(endPosition) + '%';
-      this.view.connect.setPosition(start, end, startPosition, endPosition)
+      this.view.connect.setPosition(start, end, startPosition, endPosition);
     } else {
       endPosition = 100 - parseInt(startPosition) + '%';
       startPosition = '0%';
-      this.view.connect.setPosition(start, end, startPosition, endPosition)
+      this.view.connect.setPosition(start, end, startPosition, endPosition);
     }
   }
 
   thumbHandler = (event: MouseEvent, thumb: Thumb) => {
-    const { clientXY, start, offsetWH } = this.getOrientation();
+    const { clientXY, start, offset } = this.getOrientation();
     const shiftThumb =
       event[clientXY] - thumb.element.getBoundingClientRect()[start];
 
@@ -115,7 +125,7 @@ export default class Presenter {
         newPosition = 0;
       }
       const sliderEnd =
-        this.view.sliderContainer[offsetWH] - thumb.element[offsetWH];
+        this.view.sliderContainer[offset] - thumb.element[offset];
       if (newPosition > sliderEnd) {
         newPosition = sliderEnd;
       }
@@ -127,14 +137,15 @@ export default class Presenter {
       if (thumb === this.view.thumbFrom) {
         this.model.setFrom(arr[index]);
         thumb.move(start, pos);
-        this.view.tipFrom.displayValue(arr[index])
+        this.view.tipFrom.displayValue(arr[index]);
       }
       if (thumb === this.view.thumbTo) {
         this.model.setTo(arr[index]);
         thumb.move(start, pos);
         this.view.tipTo.displayValue(arr[index]);
       }
-      this.displayConnect()
+
+      this.displayConnect();
     };
 
     function onMouseUp() {
@@ -148,9 +159,10 @@ export default class Presenter {
 
   render() {
     const props = this.getProperties();
+    this.view.render(props);
     const { start } = this.getOrientation();
     const startPos = this.calculatePosition(props.from);
-    this.view.render(props);
+
     this.view.thumbFrom.addListener(this.thumbHandler);
     this.view.thumbTo.addListener(this.thumbHandler);
     this.view.thumbFrom.move(start, startPos);
@@ -160,11 +172,10 @@ export default class Presenter {
     }
 
     if (props.tip) {
-      this.view.tipFrom.displayValue(this.model.getFrom());
-      this.view.tipTo.displayValue(this.model.getTo());
+      this.view.tipFrom.displayValue(props.from);
+      this.view.tipTo.displayValue(props.to);
     }
-
-    this.displayConnect()
+    this.displayConnect();
   }
 
   private destroy() {
