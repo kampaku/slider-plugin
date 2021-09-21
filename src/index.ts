bbec5ba7ca89ba0c95ac12c6ceadf45c5ebff323
Kampaku <43931529+kampaku@users.noreply.github.com>
2021-09-21 15:14:41 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import './style.css';
 import $ from 'jquery';
-import superSlider from './superSlider';
+// import superSlider from './superSlider';
+import './superSlider';
 import Panel from './configuration-panel/panel';
+import type Presenter from './presenter/presenter';
 
 const settings = {
   min: -10,
@@ -13,8 +15,8 @@ const settings = {
   scale: true,
 }
 
-superSlider();
-const slider = $('#root').superSlider();
+// superSlider();
+const slider: Presenter = $('#root').superSlider();
 slider.create(settings);
 slider.setMin(-100);
 slider.setMax(100);
