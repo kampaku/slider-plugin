09a7b9589e5a95742c1db13941befce5a944f659
Kampaku <43931529+kampaku@users.noreply.github.com>
2021-10-08 13:59:11 +0300

    Update thumb.ts

--- src/view/thumb.ts
+++ src/view/thumb.ts
@@ -22,4 +22,46 @@ export default class Thumb {
   move(orientation: string, value: string) {
     this.element.setAttribute('style', `${orientation}: ${value}`);
   }
+
+  width(element: HTMLElement) {
+    this.element.addEventListener('click', () => {
+      // console.log(element.offsetWidth)
+    })
+  }
+
+  slide(elementX: HTMLElement) {
+    this.element.addEventListener('pointerdown', (event: PointerEvent) => {
+      const shiftThumb =
+        event.clientX - this.element.getBoundingClientRect().left;
+
+      this.element.setPointerCapture(event.pointerId)
+      const onPointerMove = (event: PointerEvent) => {
+        event.preventDefault();
+
+        let newPosition =
+          event.clientX -
+          shiftThumb -
+          elementX.getBoundingClientRect().left;
+
+        if (newPosition < 0) {
+          newPosition = 0;
+        }
+
+        const sliderEnd =
+          elementX.offsetWidth - this.element.offsetWidth;
+
+        if (newPosition > sliderEnd) {
+          newPosition = sliderEnd;
+        }
+        this.element.style.left = newPosition + 'px'
+      };
+
+      const onPointerUp = () => {
+        this.element.removeEventListener('pointerup', onPointerUp);
+        this.element.removeEventListener('pointermove', onPointerMove);
+      }
+      this.element.addEventListener('pointermove', onPointerMove)
+      this.element.addEventListener('pointerup', onPointerUp)
+    })
+  }
 }
