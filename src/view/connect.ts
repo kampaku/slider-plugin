af4b19aad65589b47ca59084c95a21ef5237fe44
Kampaku <43931529+kampaku@users.noreply.github.com>
2021-10-28 12:35:49 +0300

    new logic for connect

--- src/view/connect.ts
+++ src/view/connect.ts
@@ -1,9 +1,12 @@
 import createElement from '../helpers/create-element';
+import type { SettingsInterface } from '../helpers/SettingsInterface';
 
 export default class Connect {
-  element: HTMLElement | null;
-  constructor() {
-    this.element = null;
+  element: HTMLElement | undefined;
+  settings: SettingsInterface;
+
+  constructor(settings: SettingsInterface) {
+    this.settings = settings;
   }
 
   create(vertical: boolean) {
@@ -14,18 +17,34 @@ export default class Connect {
       this.element.classList.add('slider-connect--horizontal');
     }
 
-    return this.element
+    return this.element;
   }
 
-  setPosition(
-    startConnect: string,
-    endConnect: string,
-    startPosition: string,
-    endPosition: string,
-  ) {
-    if (this.element) {
-      this.element.style[startConnect as 'top' | 'left' ] = startPosition;
-      this.element.style[endConnect as 'bottom' | 'right' ] = endPosition;
+  private calculatePosition(value: number) {
+    const arr = this.settings.valueArray;
+    const index = arr.indexOf(value);
+
+    return (100 / (arr.length - 1)) * index;
+  }
+
+  setPosition(from: number, to: number) {
+    if (!this.element) return;
+    const startConnect = this.settings.vertical ?
+      'top' :
+      'left';
+    const endConnect = this.settings.vertical ?
+      'bottom' :
+      'right';
+    const startPosition = this.calculatePosition(from);
+    if (this.settings.range) {
+      const endPosition = this.calculatePosition(to);
+      this.element.style[startConnect] = `${startPosition}%`;
+      this.element.style[endConnect] = `${100 - endPosition}%`;
+    } else {
+      this.element.style[startConnect] = '0%';
+      this.element.style[endConnect] = `${100 - startPosition}%`;
     }
+
+
   }
 }
