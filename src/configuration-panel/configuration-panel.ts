5358acf9b922f26ffc67ba5b5274af51fe1cd99f
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-03-08 18:14:27 +0300

    rewrite panel to pug #3

---
+++ src/configuration-panel/configuration-panel.ts
@@ -0,0 +1,117 @@
+import type SuperSlider from '../slider/SuperSlider';
+import { Events } from '../helpers/Events';
+
+import './configuration-panel.scss';
+
+type NumVal = {
+  min: (value: number) => void;
+  max: (value: number) => void;
+  step: (value: number) => void;
+  from: (value: number) => void;
+  to: (value: number) => void;
+};
+
+type BoolVal = {
+  range: (value: boolean) => void;
+  tip: (value: boolean) => void;
+  vertical: (value: boolean) => void;
+  connect: (value: boolean) => void;
+  scale: (value: boolean) => void;
+};
+
+class ConfigurationPanel {
+  slider: SuperSlider;
+  container: HTMLElement;
+  minInput!: HTMLInputElement;
+  maxInput!: HTMLInputElement;
+  stepInput!: HTMLInputElement;
+  fromInput!: HTMLInputElement;
+  toInput!: HTMLInputElement;
+  rangeInput!: HTMLInputElement;
+  tipInput!: HTMLInputElement;
+  connectInput!: HTMLInputElement;
+  scaleInput!: HTMLInputElement;
+  verticalInput!: HTMLInputElement;
+
+  constructor(slider: SuperSlider, container: HTMLElement) {
+    this.slider = slider;
+    this.container = container;
+    this.findElements();
+    this.initValues();
+    this.bindEventListener();
+    this.slider.attach(this.update.bind(this));
+  }
+
+  setValues = {
+    min: (value: number) => this.slider.setMin(value),
+    max: (value: number) => this.slider.setMax(value),
+    step: (value: number) => this.slider.setStep(value),
+    from: (value: number) => this.slider.setFrom(value),
+    to: (value: number) => this.slider.setTo(value),
+    range: (value: boolean) => this.slider.setRange(value),
+    tip: (value: boolean) => this.slider.setTip(value),
+    vertical: (value: boolean) => this.slider.setVertical(value),
+    connect: (value: boolean) => this.slider.setConnect(value),
+    scale: (value: boolean) => this.slider.setScale(value),
+  };
+
+  private findElements() {
+    this.minInput = this.container.querySelector('[data-name="min"]') as HTMLInputElement;
+    this.maxInput = this.container.querySelector('[data-name="max"]') as HTMLInputElement;
+    this.stepInput = this.container.querySelector('[data-name="step"]') as HTMLInputElement;
+    this.fromInput = this.container.querySelector('[data-name="from"]') as HTMLInputElement;
+    this.toInput = this.container.querySelector('[data-name="to"]') as HTMLInputElement;
+    this.rangeInput = this.container.querySelector('[data-name="range"]') as HTMLInputElement;
+    this.tipInput = this.container.querySelector('[data-name="tip"]') as HTMLInputElement;
+    this.connectInput = this.container.querySelector('[data-name="connect"]') as HTMLInputElement;
+    this.scaleInput = this.container.querySelector('[data-name="scale"]') as HTMLInputElement;
+    this.verticalInput = this.container.querySelector('[data-name="vertical"]') as HTMLInputElement;
+  }
+
+  private initValues() {
+    this.minInput.value = String(this.slider.getMin());
+    this.maxInput.value = String(this.slider.getMax());
+    this.stepInput.value = String(this.slider.getStep());
+    this.fromInput.value = String(this.slider.getFrom());
+    this.toInput.value = String(this.slider.getTo());
+    this.rangeInput.checked = this.slider.getRange();
+    this.tipInput.checked = this.slider.getTip();
+    this.connectInput.checked = this.slider.getConnect();
+    this.scaleInput.checked = this.slider.getScale();
+    this.verticalInput.checked = this.slider.getVertical();
+
+    this.toInput.disabled = !this.rangeInput.checked;
+  }
+
+  private setValue = ({ target }: Event) => {
+    if (!target) return;
+    const input = (<HTMLInputElement>target).closest('[data-name]') as HTMLInputElement;
+    if (!input) return;
+
+    const name = input.dataset.name;
+
+    if (!name) return;
+
+    if (input.type === 'number') {
+      const value = Number(input.value);
+      this.setValues[name as keyof NumVal](value);
+    } else if (input.type === 'checkbox') {
+      const checked = !input.checked;
+      this.setValues[name as keyof BoolVal](!checked);
+    }
+  };
+
+  private update(eventName: Events) {
+    const shouldUpdate = eventName === Events.changeFrom
+      || eventName === Events.changeTo
+      || eventName === Events.update;
+    if (!shouldUpdate) return;
+    this.initValues();
+  }
+
+  private bindEventListener() {
+    this.container.addEventListener('change', this.setValue);
+  }
+}
+
+export default ConfigurationPanel;
--- src/index.ts
+++ src/index.ts
@@ -3,70 +3,52 @@ import $ from 'jquery';
 import '../public/index.scss';
 
 import './jqueryPlugin';
-import Panel from './configuration-panel/panel';
+import ConfigurationPanel from './configuration-panel/configuration-panel';
 
-const settings = {
-  min: -10,
-  max: 10,
-  step: 1,
-  from: 0,
-  to: 8,
-  range: true,
-  tip: true,
-  connect: true,
-  scale: true,
-};
-const settings2 = {
-  min: -10,
-  max: 10,
-  step: 1,
-  from: 0,
-  to: 8,
-  range: true,
-  tip: true,
-  connect: false,
-  scale: true,
-  vertical: true,
-};
+const settings = [
+  {
+    min: -10,
+    max: -1,
+  },
+  {
+    min: -10,
+    max: 10,
+    step: 1,
+    from: 0,
+    to: 8,
+    range: true,
+    tip: true,
+    connect: false,
+    scale: false,
+    vertical: true,
+  },
+  {
+    min: -10,
+    max: 10,
+    step: 2,
+    from: 0,
+    range: false,
+    tip: true,
+    connect: true,
+    scale: false,
+  },
+  {
+    min: 0,
+    max: 100,
+    step: 1,
+    from: 55,
+    range: false,
+    tip: true,
+    connect: false,
+    scale: false,
+  },
+];
 
-const settings3 = {
-  min: -10,
-  max: 10,
-  step: 2,
-  from: 0,
-  range: false,
-  tip: true,
-  connect: true,
-  scale: false,
-};
+const plugins = document.querySelectorAll('.js-wrapper') as NodeListOf<HTMLElement>;
+const sliders = document.querySelectorAll('.js-slider-container') as NodeListOf<HTMLElement>;
 
-const settings4 = {
-  min: 0,
-  max: 100,
-  step: 1,
-  from: 55,
-  range: false,
-  tip: true,
-  connect: false,
-  scale: false,
-};
-
-const plugin = document.querySelector('.js-wrapper1') as HTMLElement;
-const plugin2 = document.querySelector('.js-wrapper2') as HTMLElement;
-const plugin3 = document.querySelector('.js-wrapper3') as HTMLElement;
-const plugin4 = document.querySelector('.js-wrapper4') as HTMLElement;
-const slider1Container = $('.js-slider1');
-const slider2Container = $('.js-slider2');
-const slider3Container = $('.js-slider3');
-const slider4Container = $('.js-slider4');
-
-const slider = slider1Container.superSlider(settings);
-const slider2 = slider2Container.superSlider(settings2);
-const slider3 = slider3Container.superSlider(settings3);
-const slider4 = slider4Container.superSlider(settings4);
-
-new Panel(slider, plugin);
-new Panel(slider2, plugin2);
-new Panel(slider3, plugin3);
-new Panel(slider4, plugin4);
-
+settings.forEach((setting, i) => {
+  const slider = $(sliders[i]).superSlider(setting);
+  const wrapper = plugins[i];
+  new ConfigurationPanel(slider, wrapper);
+});
