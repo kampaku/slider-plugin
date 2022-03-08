4afde79d0abd7d99b38f7d7a2de5a02ad053e0f7
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-03-08 18:13:12 +0300

    delete unnecessary code

--- jest.config.ts
+++ jest.config.ts
@@ -1,27 +1,10 @@
 import { defaults } from 'jest-config';
 import type { Config } from '@jest/types';
 
-// Синхронно загружаемый конфиг
 const config: Config.InitialOptions = {
-  // moduleFileExtensions: [
-  //   ...defaults.moduleFileExtensions,
-  //   'ts',
-  //   'tsx',
-  //   'json',
-  //   'node',
-  // ],
   verbose: true,
-  // extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
   moduleNameMapper: { "\\.(css|scss|less)$": "<rootDir>/test/__mocks__/styleMock.js" }
 };
 export default config;
-
-// // Или с помощью асинхронной функции
-// export default async (): Promise<Config.InitialOptions> => {
-//   return {
-//     verbose: true,
-//     preset: 'ts-jest'
-//   };
-// };
