097cff77a3c26f3cdba6684f1c6acd31b487fcfc
Kampaku <43931529+kampaku@users.noreply.github.com>
2022-03-08 18:12:20 +0300

    rename

--- src/helpers/isCrossed.ts
+++ src/helpers/isCrossed.ts
@@ -1,4 +1,4 @@
-function isCross(elemOne: HTMLElement, elemTwo: HTMLElement) {
+function isCrossed(elemOne: HTMLElement, elemTwo: HTMLElement) {
   const leftElemEnd = elemOne.getBoundingClientRect().right;
   const leftElemBottom = elemOne.getBoundingClientRect().bottom;
   const rightElemStart = elemTwo.getBoundingClientRect().left;
@@ -6,4 +6,4 @@ function isCross(elemOne: HTMLElement, elemTwo: HTMLElement) {
   return (leftElemEnd > rightElemStart) && (leftElemBottom > rightElemTop);
 }
 
-export default isCross;
+export default isCrossed;
